1db7194c17c6cc60c5d0b9005a16b7e7a556a198
soohwan93 <soohwan2im@gmail.com>
2024-02-24 14:12:57 +0900

    임시 메인페이지 및 각자의 개발환경 구성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./component/Main/Main";
+import { Kim } from "./component/test/Kim";
+import { Yang } from "./component/test/Yang";
+import { Yun } from "./component/test/Yun";
+import { Choi } from "./component/test/Choi";
+import { Hong } from "./component/test/Hong";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/kim" element={<Kim />} />
+        <Route path="/yang" element={<Yang />} />
+        <Route path="/yun" element={<Yun />} />
+        <Route path="/choi" element={<Choi />} />
+        <Route path="/hong" element={<Hong />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/component/test/Yang.jsx
@@ -0,0 +1,3 @@
+export function Yang() {
+  return <h1>여기에 자유롭게 수정하면서 테스트 해보시면 됩니다!</h1>;
+}
