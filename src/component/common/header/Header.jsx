469d16119051e80133094a3829a4c74dbe275f4c
soohwan93 <soohwan2im@gmail.com>
2024-02-26 07:09:53 +0900

    header 컴포넌트 개발 완료

---
+++ src/component/common/header/Header.jsx
@@ -0,0 +1,25 @@
+import styles from "./Header.module.css";
+import heaferIcon from "../../../asset/img/headerIcon/rollingIcon.png";
+
+const HEADER_LOGO_TEXT = "Rolling";
+const HEADER_BUTTON_TEXT = "롤링 페이퍼 만들기";
+function Header() {
+  return (
+    <>
+      <div className={styles.header}>
+        <div className={styles.headerContainer}>
+          <div className={styles.logoContainer}>
+            <img src={heaferIcon} alt="headerIcon" />
+            <span className={styles.logoText}>{HEADER_LOGO_TEXT}</span>
+          </div>
+          <button className={styles.button}>
+            <span className={styles.buttonText}>{HEADER_BUTTON_TEXT}</span>
+          </button>
+        </div>
+      </div>
+      <div className={styles.line} />
+    </>
+  );
+}
+
+export default Header;
