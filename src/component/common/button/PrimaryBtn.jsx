7253ffc3a5e600b05048596c5efcdbc825c12650
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-26 18:42:54 +0900

    style: console.log 제거

--- src/component/common/button/PrimaryBtn.jsx
+++ src/component/common/button/PrimaryBtn.jsx
@@ -1,7 +1,6 @@
 import Button from "./Button";
 
 const PrimaryBtn = ({ children, width, height, disabled }) => {
-  console.log(typeof width);
   return disabled ? (
     <Button width={width} height={height} color="primary" disabled>
       {children}
