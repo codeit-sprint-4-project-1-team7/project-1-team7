e1226c7ba78d1a114c67eabb680c1551f9e3674b
양서연 <ysynop7@naver.com>
2024-02-26 18:27:05 +0900

    option 컴포넌트 개발

---
+++ src/component/common/option/ColorOption.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import checkIcon from '../../../asset/img/optionIcon/check_Icon.png';
+import styles from './ColorOption.module.css';
+
+function ColorOption() {
+  const [clickItem, setClickItem] = useState('orange');
+
+  const handleClick = (e) => {
+    setClickItem(e.target.value === clickItem ? '' : e.target.value);
+  }
+
+  return (
+    <div className={styles.backgroundOptions}>
+      <button type='button' value='orange' className={styles.backgroundOrange} onClick={handleClick}>
+        {'orange' === clickItem && <img src={checkIcon} alt="check" />}
+      </button>
+
+      <button type='button' value='purple' className={styles.backgroundPurple} onClick={handleClick}>
+        {'purple' === clickItem && <img src={checkIcon} alt="check" />}
+      </button>
+
+      <button type='button' value='blue' className={styles.backgroundBlue} onClick={handleClick}>
+        {'blue' === clickItem && <img src={checkIcon} alt="check" />}
+      </button>
+
+      <button type='button' value='green' className={styles.backgroundGreen} onClick={handleClick}>
+        {'green' === clickItem && <img src={checkIcon} alt="check" />}
+      </button>
+    </div>
+  );
+}
+
+export default ColorOption;
--- src/component/test/Yang.jsx
+++ src/component/test/Yang.jsx
@@ -1,3 +1,12 @@
+import ColorOption from "../common/option/ColorOption";
+import UserProfileOption from "../common/option/UserProfileOption";
+
 export function Yang() {
-  return <h1>여기에 자유롭게 수정하면서 테스트 해보시면 됩니다!</h1>;
+  return (
+    <>
+      <h1>여기에 자유롭게 수정하면서 테스트 해보시면 됩니다!</h1>
+      <UserProfileOption />
+      <ColorOption />
+    </>
+  )
 }
