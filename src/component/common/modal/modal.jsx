701dc3c4faa43dd594c1df73035c97aee1462103
hong <hsw5971@naver.com>
2024-02-26 15:48:27 +0900

    modal 컴포넌트 임시 구현

---
+++ src/component/common/modal/modal.jsx
@@ -0,0 +1,47 @@
+import styles from './modal.module.css';
+
+const tempName = '홍성욱';
+const tempBadge = '임시 배지';
+const tempDate = '임시 날짜';
+
+export const Modal = () => {
+  const profileName = `From. ${tempName}`;
+  return (
+    <div className={styles.modalBackground}>
+      <div className={styles.modalContents}>
+        <div className={styles.modalHeader}>
+          <div className={styles.modalProfile}>
+            <div className={styles.profileImg} alt="임시 프로필 이미지"></div>
+            <div className={styles.profileForm}>
+              <div className={styles.profileName}>{profileName}</div>
+              <div>{tempBadge}</div>
+            </div>
+          </div>
+          <div className={styles.modalDate}>{tempDate}</div>
+        </div>
+        <div className={styles.modalMessageForm}>
+          <div className={styles.modalMessage}>
+            임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시
+            메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지
+            입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다.
+            임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시
+            메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지
+            입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다.
+            임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시
+            메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지
+            입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다.
+            임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시
+            메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지
+            입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다.
+            임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시
+            메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지
+            입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다.
+            임시 메시지 입니다. 임시 메시지 입니다. 임시 메시지 입니다. 임시
+            메시지 입니다.
+          </div>
+        </div>
+        <button>확인</button>
+      </div>
+    </div>
+  );
+};
--- src/component/test/Hong.jsx
+++ src/component/test/Hong.jsx
@@ -1,3 +1,9 @@
+import { Modal } from '../common/modal/modal';
+
 export function Hong() {
-  return <h1>여기에 자유롭게 수정하면서 테스트 해보시면 됩니다!</h1>;
+  return (
+    <>
+      <Modal></Modal>
+    </>
+  );
 }
