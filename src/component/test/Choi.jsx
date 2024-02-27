190041319ade97385fda94c8b9857f6904fc1051
choiwonseokgit <cs0328@hanmail.net>
2024-02-27 23:38:03 +0900

    Feat: TextArea 컴포넌트 생성 완료

--- src/component/test/Choi.jsx
+++ src/component/test/Choi.jsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 import Input from "../common/textField/Input/Input";
 import SelectBox from "../common/textField/SelectBox/SelectBox";
 import { SELECT_TYPE } from "../common/textField/SelectBox/selectType";
+import TextArea from "../common/textField/TextArea/TextArea";
+import DOMPurify from "dompurify";
 
 export function Choi() {
   const [inputValue, setInputValue] = useState("");
   const [selectValue, setSelectValue] = useState("");
+  const [quillValue, setQuillValue] = useState("");
 
   const handleSelectValue = (value) => {
     setSelectValue(value);
   };
   const handleInputValue = (value) => {
     setInputValue(value);
+  };
+  const handleQuillValue = (value) => {
+    setQuillValue(value);
   };
 
   return (
@@ -26,7 +32,30 @@ export function Choi() {
         onSelectValueChange={handleSelectValue}
         selectType={SELECT_TYPE.relation}
       />
+      <br />
+      <div
+        dangerouslySetInnerHTML={{
+          __html: DOMPurify.sanitize(quillValue),
+        }}
+        className="dompurifyBox"
+      />
+
+      <br />
+      <div style={{ fontSize: "16px" }}>{quillValue}</div>
+      <br />
+      <TextArea onQuillValueChange={handleQuillValue} />
       <h1>여기에 자유롭게 수정하면서 테스트 해보시면 됩니다!</h1>
+
+      <ol>
+        <li style={{ fontSize: "16px" }}>asdfsf</li>
+        <li>2adfasf</li>
+        <li>3asfddasf</li>
+      </ol>
+      <ul>
+        <li style={{ fontSize: "16px" }}>asdfsf</li>
+        <li>2adfasf</li>
+        <li>3asfddasf</li>
+      </ul>
     </div>
   );
 }
