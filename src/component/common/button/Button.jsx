4b375b18bd4aaa66d96aa12ee4b9a1e20ad8f68c
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-27 15:52:33 +0900

    refactor: 필요없는 prop 제거, px사이즈 수정

--- src/component/common/button/Button.jsx
+++ src/component/common/button/Button.jsx
@@ -1,18 +1,15 @@
-import "./tempReset.css";
 import styles from "./Button.module.css";
 
 const Button = ({
   children,
   width,
   height = "standard",
-  bold,
-  color,
   shape,
   disabled,
 }) => {
   const className = `${styles.btn} ${styles[height]} ${styles[color]} ${
     shape ? styles[shape] : ""
-  } ${bold ? styles.bold : ""}`;
+  }`;
   const style = width ? { width: `${width}` } : {};
 
   return (
