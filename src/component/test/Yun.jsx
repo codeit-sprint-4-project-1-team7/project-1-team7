5b9ad36e1a0fc3d1c37e9b708bea0a961a361a86
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-28 05:17:54 +0900

    test: 버튼 컴포넌트 테스트 코드 변경

--- src/component/test/Yun.jsx
+++ src/component/test/Yun.jsx
@@ -1,105 +1,56 @@
-import add from "../../asset/img/buttonIcon/add-24.png";
-import plus from "../../asset/img/buttonIcon/plus.png";
-import deleted from "../../asset/img/buttonIcon/deleted.png";
-import arrowRight from "../../asset/img/buttonIcon/arrow-right.png";
 import Button from "../common/button/Button";
-import ToggleButton from "../common/button/ToggleButton";
 
 export function Yun() {
   return (
     <>
       <h1>여기에 자유롭게 수정하면서 테스트 해보시면 됩니다!</h1>
-      <Button type="dark" height="tall" shape="circle">
-        <img src={plus} alt="더하기" />
-      </Button>
-
-      <br />
-
-      <Button type="dark" height="tall" shape="circle" disabled>
-        <img src={plus} alt="더하기" />
-      </Button>
-
-      <br />
-
-      <Button type="arrow" height="standard" shape="circle" disabled>
-        <img src={arrowRight} alt="오른쪽 화살표" />
-      </Button>
-
-      <br />
-
-      <Button type="outlined" height="compact" shape="square">
-        <img src={deleted} alt="휴지통" />
-      </Button>
-
-      <br />
-
-      <Button type="outlined" height="compact" shape="square" disabled>
-        <img src={deleted} alt="휴지통" />
-      </Button>
-
-      <br />
-      <br />
-
-      <Button type="primary" width="280px" height="standard">
-        Primary_width280
-      </Button>
-
-      <br />
-      <br />
-
-      <Button type="primary" height="standard">
-        Primary
-      </Button>
-
-      <br />
-      <br />
-
-      <Button type="primary" height="standard" disabled>
-        Primary_disabled
-      </Button>
-
-      <br />
-      <br />
-
-      <Button type="secondary" height="standard">
-        Secondary
-      </Button>
-
-      <br />
-      <br />
-
-      <Button type="secondary" height="standard" disabled>
-        Secondary_disabled
-      </Button>
-
-      <br />
       <br />
 
       <Button type="outlined" height="standard">
-        <img src={add} alt="추가" />
-        Outlined
+        롤링 페이퍼 만들기
       </Button>
 
       <br />
-      <br />
 
-      <Button type="outlined" height="standard" disabled>
-        Outlined_disabled
+      <Button type="primary" width="280">
+        구경해보기
       </Button>
 
       <br />
+
+      <Button type="outlined" height="short" icon="add">
+        추가
+      </Button>
+
       <br />
 
-      <ToggleButton />
+      <Button type="outlined" height="short" icon="share" />
 
       <br />
+
+      <Button type="circle" icon="plus" />
+
       <br />
+
+      <Button type="primary" width="120" height="standard">
+        확인
+      </Button>
+
       <br />
+
+      <Button type="primary" width="92" height="standard">
+        삭제하기
+      </Button>
+
       <br />
+
+      <Button type="outlined" height="standard" icon="delete" />
+
       <br />
-      <br />
-      <br />
-      <br />
+
+      <Button type="circle" icon="check" />
+
+      <Button type="toggle" />
     </>
   );
 }
