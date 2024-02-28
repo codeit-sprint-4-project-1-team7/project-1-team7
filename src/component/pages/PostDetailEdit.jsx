0b41a6fca5cdc37e60c8debb58353db29f28dcfd
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-28 23:18:38 +0900

    feat: Edit 페이지에 카드 컴포넌트 추가

--- src/component/pages/PostDetailEdit.jsx
+++ src/component/pages/PostDetailEdit.jsx
@@ -1,5 +1,23 @@
 function PostDetailEdit() {
-  return <div>PostDetailEdit</div>;
+  return (
+    <div>
+      <h1>PostDetailEdit</h1>
+      <div className={styles.cardListContainer}>
+        <div className={styles.cardList}>
+          {messages.map(({ id, sender, relationship, content, createdAt }) => (
+            <Card
+              key={id}
+              sender={sender}
+              relationship={relationship}
+              content={content}
+              createdAt={createdAt}
+              onButtonClick={() => onDeleteButtonClick(id)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PostDetailEdit;
