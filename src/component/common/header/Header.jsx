f83b85e305b72c535afe0cfe4b0bc58c82fa2f3a
soohwan93 <soohwan2im@gmail.com>
2024-02-28 17:56:19 +0900

    Feat: 롤링페이퍼 만들기 버튼 페이지에 따라 보이거나 안 보이는 기능 추가

--- src/component/common/header/Header.jsx
+++ src/component/common/header/Header.jsx
@@ -1,10 +1,13 @@
 import styles from "./Header.module.css";
 import heaferIcon from "../../../asset/img/headerIcon/rolling-icon.png";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const HEADER_LOGO_TEXT = "Rolling";
 const HEADER_BUTTON_TEXT = "롤링 페이퍼 만들기";
 function Header() {
+  const [isMakeRollingPaperVisible, setIsMakeRollingPaperVisible] =
+    useState(false);
   const navigation = useNavigate();
 
   const handleLogoClick = () => {
@@ -14,6 +17,14 @@ function Header() {
   const handleRollingPaperButtonClick = () => {
     navigation("/post");
   };
+  useEffect(() => {
+    const searchUrl = window.location.href;
+    if (searchUrl.indexOf("post") !== -1) {
+      setIsMakeRollingPaperVisible(false);
+    } else {
+      setIsMakeRollingPaperVisible(true);
+    }
+  }, [isMakeRollingPaperVisible]);
   return (
     <>
       <div className={styles.header}>
@@ -22,12 +33,14 @@ function Header() {
             <img src={heaferIcon} alt="headerIcon" />
             <span className={styles.logoText}>{HEADER_LOGO_TEXT}</span>
           </div>
-          <div
-            onClick={handleRollingPaperButtonClick}
-            className={styles.button}
-          >
-            <span className={styles.buttonText}>{HEADER_BUTTON_TEXT}</span>
-          </div>
+          {isMakeRollingPaperVisible && (
+            <div
+              onClick={handleRollingPaperButtonClick}
+              className={styles.button}
+            >
+              <span className={styles.buttonText}>{HEADER_BUTTON_TEXT}</span>
+            </div>
+          )}
         </div>
       </div>
       <div className={styles.line} />
