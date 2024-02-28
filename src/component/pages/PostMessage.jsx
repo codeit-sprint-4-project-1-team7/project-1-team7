6125e9929a99e4b067bdcc2694cf60a322dc3e2b
choiwonseokgit <cs0328@hanmail.net>
2024-02-28 16:32:24 +0900

    Feat: PostMessage에 Button추가

--- src/component/pages/PostMessage.jsx
+++ src/component/pages/PostMessage.jsx
@@ -1,11 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Input from "../common/textField/input/Input";
 import SelectBox from "../common/textField/selectBox/SelectBox";
 import TextArea from "../common/textField/textArea/TextArea";
 import { SELECT_TYPE } from "../common/textField/selectBox/selectType";
 import { getProfileImagesApiResponse } from "../../util/api";
 import styles from "./PostMessage.module.css";
-// import DOMPurify from "dompurify";
+import Button from "../common/button/Button";
+import DOMPurify from "dompurify";
 
 function PostMessage() {
   const [inputValue, setInputValue] = useState("");
@@ -13,6 +14,7 @@ function PostMessage() {
   const [currentFont, setCurrentFont] = useState("");
   const [quillValue, setQuillValue] = useState("");
   const [profileImgList, setProfileImgList] = useState([]);
+  const textAreaRef = useRef(null);
 
   const handleInputValue = (value) => {
     setInputValue(value);
@@ -32,36 +34,63 @@ function PostMessage() {
     setProfileImgList(res.imageUrls);
   };
 
+  const changeFontFamily = (style) => {
+    textAreaRef.current.style.fontFamily = style;
+  };
+
+  console.log(currentFont);
+
   useEffect(() => {
     getImgProfileList();
-  }, []);
+    changeFontFamily(currentFont);
+  }, [currentFont]);
 
   return (
     <div className={styles.container}>
-      <div>From</div>
-      <Input inputValue={inputValue} onInputValueChange={handleInputValue} />
+      <div className={styles.inputText}>From.</div>
+      <div className={styles.inputContainer}>
+        <Input inputValue={inputValue} onInputValueChange={handleInputValue} />
+      </div>
 
-      <div>프로필 이미지</div>
+      <div className={styles.profileText}>프로필 이미지</div>
       {profileImgList?.map((profileImg, i) => (
-        <img key={i} src={profileImg} alt="profileImg" />
+        <img className={styles.img} key={i} src={profileImg} alt="profileImg" />
       ))}
 
-      <div>상대와의 관계</div>
-      <SelectBox
-        selectValue={currentRelation}
-        onSelectValueChange={handleCurrentRelation}
-        selectType={SELECT_TYPE.relation}
-      />
+      <div className={styles.selectBoxText}>상대와의 관계</div>
+      <div className={styles.selectBoxContainer}>
+        <SelectBox
+          selectValue={currentRelation}
+          onSelectValueChange={handleCurrentRelation}
+          selectType={SELECT_TYPE.relation}
+        />
+      </div>
 
-      <div>내용을 입력해주세요</div>
-      <TextArea onQuillValueChange={handleQuillValue} />
-
-      <div>폰트선택</div>
-      <SelectBox
-        selectValue={currentFont}
-        onSelectValueChange={handleCurrentFont}
-        selectType={SELECT_TYPE.font}
+      <div
+        ref={textAreaRef}
+        dangerouslySetInnerHTML={{
+          __html: DOMPurify.sanitize(quillValue),
+        }}
+        className="dompurifyBox"
       />
+      <div>{quillValue}</div>
+
+      <div className={styles.textAreaText}>내용을 입력해주세요</div>
+      <div className={styles.textAreaContainer}>
+        <TextArea onQuillValueChange={handleQuillValue} />
+      </div>
+
+      <div className={styles.selectBoxText}>폰트선택</div>
+      <div className={styles.selectBoxContainer}>
+        <SelectBox
+          selectValue={currentFont}
+          onSelectValueChange={handleCurrentFont}
+          selectType={SELECT_TYPE.font}
+        />
+      </div>
+      <Button type={"primary"} width={"100%"}>
+        생성하기
+      </Button>
     </div>
   );
 }
