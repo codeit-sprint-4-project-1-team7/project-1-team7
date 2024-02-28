685f01ac5a62fede0cd9f5ff55ef0a13aba3ae01
양서연 <ysynop7@naver.com>
2024-02-28 14:42:22 +0900

    Feat: post 페이지 컴포넌트 배치 및 구현

--- src/component/pages/Post.jsx
+++ src/component/pages/Post.jsx
@@ -1,5 +1,33 @@
+import { useState } from "react";
+import styles from "./Post.module.css";
+import Input from "../common/textField/Input/Input";
+import ColorOption from "../common/option/ColorOption";
+import Button from "../common/button/Button";
+
 function Post() {
-  return <div>Post</div>;
+  const [inputValue, setInputValue] = useState("")
+
+  const handleInputValue = (value) => setInputValue(value);
+  
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.inputBox}>
+          <div className={styles.inputName}>To.{inputValue}</div>
+          <Input inputValue={inputValue} onInputValueChange={handleInputValue}/>
+        </div>
+
+        <div className={styles.describeBackgroundInfo}>
+          <h2 className={styles.describeTitle}>배경화면을 선택해 주세요</h2>
+          <p className={styles.describeSubtitle}>컬러를 선택하거나, 이미지를 선택할 수 있습니다.</p>
+        </div>
+        <div className={styles.selectButton}>
+          <Button type="toggle" />
+        </div>
+        <ColorOption />
+      </div>
+    </div>
+  )
 }
 
 export default Post;
