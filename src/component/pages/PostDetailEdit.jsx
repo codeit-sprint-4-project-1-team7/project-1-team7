43e6f1034d8e4bf4582752d5d90cad233a62bd1e
ayoung-iya <rhaehfdl1879@naver.com>
2024-02-28 23:21:43 +0900

    test: 데이터 정보를 전달할 수 있도록 카드 컴포넌트 복사 및 props 추가
    해당 코드는 sample로 사용 후 나중에 제거 예정

--- src/component/pages/PostDetailEdit.jsx
+++ src/component/pages/PostDetailEdit.jsx
@@ -1,3 +1,39 @@
+import Button from "../common/button/Button";
+// sampleCard
+function Card({ sender, relationship, content, createdAt, onButtonClick }) {
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.profileContainer}>
+        <div className={styles.profile}>
+          <div className={styles.img} />
+          <div className={styles.nameAndBadgeContainer}>
+            <div className={styles.nameContainer}>
+              <span>From.</span>
+              <span>{sender}</span>
+            </div>
+            <div className={styles.badge}>
+              <div className={styles.badgeText}>{relationship}</div>
+            </div>
+          </div>
+          <div className={styles.trashButton}>
+            <Button
+              type="outlined"
+              height="standard"
+              icon="delete"
+              onClick={onButtonClick}
+            />
+          </div>
+        </div>
+        <div className={styles.line} />
+      </div>
+      <div className={styles.textAreaContainer}>
+        <div className={styles.textBox}>{content}</div>
+        <span className={styles.date}>{createdAt}</span>
+      </div>
+    </div>
+  );
+}
+
 function PostDetailEdit() {
   return (
     <div>
