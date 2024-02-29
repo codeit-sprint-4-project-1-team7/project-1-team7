c0ec155f6178e84b7ef0ed795ac12da7baf7691e
양서연 <ysynop7@naver.com>
2024-02-29 15:34:59 +0900

    Refactor: css 공통화 및 코드 정리

--- src/component/common/option/ColorOption.jsx
+++ src/component/common/option/ColorOption.jsx
@@ -3,7 +3,7 @@ import checkIcon from '../../../asset/img/optionIcon/check_Icon.png';
 import styles from './ColorOption.module.css';
 
 function ColorOption() {
-  const [clickItem, setClickItem] = useState('orange');
+  const [clickItem, setClickItem] = useState('beige');
 
   const handleClick = (e) => {
     setClickItem(e.target.value === clickItem ? '' : e.target.value);
@@ -11,20 +11,41 @@ function ColorOption() {
 
   return (
     <div className={styles.backgroundOptions}>
-      <button type='button' value='orange' className={styles.backgroundOrange} onClick={handleClick}>
-        {'orange' === clickItem && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
+      <button
+        type='button'
+        value='beige'
+        className={`${styles.option} ${styles.colorBeige}`}
+        onClick={handleClick}>
+        {'beige' === clickItem
+          && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
       </button>
 
-      <button type='button' value='purple' className={styles.backgroundPurple} onClick={handleClick}>
-        {'purple' === clickItem && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
+      <button
+        type='button'
+        value='purple'
+        className={`${styles.option}
+        ${styles.colorPurple}`}
+        onClick={handleClick}>
+        {'purple' === clickItem
+          && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
       </button>
 
-      <button type='button' value='blue' className={styles.backgroundBlue} onClick={handleClick}>
-        {'blue' === clickItem && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
+      <button
+        type='button'
+        value='blue'
+        className={`${styles.option} ${styles.colorBlue}`}
+        onClick={handleClick}>
+        {'blue' === clickItem
+          && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
       </button>
 
-      <button type='button' value='green' className={styles.backgroundGreen} onClick={handleClick}>
-        {'green' === clickItem && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
+      <button
+        type='button'
+        value='green'
+        className={`${styles.option} ${styles.colorGreen}`}
+        onClick={handleClick}>
+        {'green' === clickItem
+          && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
       </button>
     </div>
   );
