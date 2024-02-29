656a749014dad93313d02cbb73c737c0a24cb31e
hong <hsw5971@naver.com>
2024-02-29 14:10:10 +0900

    Design: 폰트, 색상 변수 재적용

--- src/component/pages/List.jsx
+++ src/component/pages/List.jsx
@@ -1,5 +1,21 @@
+import Button from '../common/button/Button';
+import styles from './List.module.css';
 function List() {
-  return <div>List</div>;
+  return (
+    <div className={styles.listBackground}>
+      <div className={styles.cardListForm}>
+        <h1>인기 롤링 페이퍼 🔥</h1>
+        <div>card list</div>
+      </div>
+      <div className={styles.cardListForm}>
+        <h1>최근에 만든 롤링 페이퍼 ⭐️️</h1>
+        <div>card list</div>
+      </div>
+      <div>
+        <Button type="primary">나도 만들어보기</Button>
+      </div>
+    </div>
+  );
 }
 
 export default List;
