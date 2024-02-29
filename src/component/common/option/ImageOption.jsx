ecc775a6f14ef0b8964b2aeb0833092311529542
양서연 <ysynop7@naver.com>
2024-02-29 15:33:51 +0900

    Fix: 이미지 옵션 선택했을 때, 체크 표시와 함께 배경 블러 처리

--- src/component/common/option/ImageOption.jsx
+++ src/component/common/option/ImageOption.jsx
@@ -1,31 +1,57 @@
 import { useState } from "react";
 import styles from "./ImageOption.module.css";
 import checkIcon from "../../../asset/img/optionIcon/check_Icon.png";
+import sample01 from "../../../asset/img/optionIcon/sample_Image_01.jpg";
+import sample02 from "../../../asset/img/optionIcon/sample_Image_02.jpg";
 
 function ImageOption() {
   const [clickImage, setClickImage] = useState('baseImg1');
 
   const handleClick = (e) => {
     setClickImage(e.target.value === clickImage ? '' : e.target.value);
-    console.log(e.target.value);
   }
 
+  const CHECKED_BACKGROUND = 'linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5))';
+
+
   return (
-    <div className={styles.ImgOptions}>
-      <button className={styles.baseImg01} type='button' value='baseImg1' onClick={handleClick}>
-        {'baseImg1' === clickImage && <img className={styles.ImgChecked} src={checkIcon} alt="check" />}
+    <div className={styles.imgOptions}>
+      <button
+        style={'baseImg1' === clickImage ? {backgroundImage: `${CHECKED_BACKGROUND}, url(${sample01})`} : {backgroundImage: `url(${sample01})`}}
+        className={styles.option}
+        type='button' value='baseImg1'
+        onClick={handleClick}>
+        {'baseImg1' === clickImage
+          && <img className={styles.ImgChecked} src={checkIcon} alt="check" />}
       </button>
 
-      <button className={styles.baseImg02} type='button' value='baseImg2' onClick={handleClick}>
-        {'baseImg2' === clickImage && <img className={styles.ImgChecked} src={checkIcon} alt="check" />}
+      <button
+        style={'baseImg2' === clickImage ? {backgroundImage: `${CHECKED_BACKGROUND}, url(${sample02})`} : {backgroundImage: `url(${sample02})`}}
+        className={styles.option}
+        type='button'
+        value='baseImg2'
+        onClick={handleClick}>
+        {'baseImg2' === clickImage
+          && <img className={styles.ImgChecked} src={checkIcon} alt="check" />}
       </button>
 
-      <button className={styles.baseImg03} type='button' value='baseImg3' onClick={handleClick}>
-        {'baseImg3' === clickImage && <img className={styles.ImgChecked} src={checkIcon} alt="check" />}
+      <button
+        style={'baseImg3' === clickImage ? {backgroundImage: `${CHECKED_BACKGROUND}, url(${sample01})`} : {backgroundImage: `url(${sample01})`}}
+        className={styles.option}
+        type='button'
+        value='baseImg3'
+        onClick={handleClick}>
+        {'baseImg3' === clickImage
+          && <img className={styles.ImgChecked} src={checkIcon} alt="check" />}
       </button>
 
-      <button className={styles.baseImg04} type='button' value='baseImg4' onClick={handleClick}>
-        {'baseImg4' === clickImage && <img className={styles.ImgChecked} src={checkIcon} alt="check" />}
+      <button
+        style={'baseImg4' === clickImage ? {backgroundImage: `${CHECKED_BACKGROUND}, url(${sample02})`} : {backgroundImage: `url(${sample02})`}}
+        className={styles.option}
+        type='button' value='baseImg4'
+        onClick={handleClick}>
+        {'baseImg4' === clickImage
+          && <img className={styles.ImgChecked} src={checkIcon} alt="check" />}
       </button>
     </div>
   );
