53c64a53bb0d60c826382bd5fb3332be30b00c1d
hong <hsw5971@naver.com>
2024-02-29 14:14:59 +0900

    Feat: List 페이지 나도 만들어보기 버튼 Post 페이지로 연결-> useNavigate 사용

--- src/component/pages/List.jsx
+++ src/component/pages/List.jsx
@@ -1,6 +1,9 @@
 import Button from '../common/button/Button';
 import styles from './List.module.css';
+import { useNavigate } from 'react-router-dom';
+
 function List() {
+  const navigate = useNavigate();
   return (
     <div className={styles.listBackground}>
       <div className={styles.cardListForm}>
@@ -11,8 +14,15 @@ function List() {
         <h1>최근에 만든 롤링 페이퍼 ⭐️️</h1>
         <div>card list</div>
       </div>
-      <div>
-        <Button type="primary">나도 만들어보기</Button>
+      <div className={styles.listBottom}>
+        <Button
+          type="primary"
+          onClick={() => {
+            navigate('/post');
+          }}
+        >
+          나도 만들어보기
+        </Button>
       </div>
     </div>
   );
