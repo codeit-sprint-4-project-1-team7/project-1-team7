284819fc17fe288fec8e9caca395cb6dcb44d37b
hong <hsw5971@naver.com>
2024-03-02 13:26:20 +0900

    Feat: list 페이지 개발 진행중

--- src/component/pages/List.jsx
+++ src/component/pages/List.jsx
@@ -1,18 +1,44 @@
 import Button from '../common/button/Button';
 import styles from './List.module.css';
 import { useNavigate } from 'react-router-dom';
+import CardList from '../common/cardList/CardList';
+import { CardlistTest } from './CardlistTest';
+import { useRef } from 'react';
 
 function List() {
   const navigate = useNavigate();
+  const containerRef = useRef(null);
+
+  const handleScroll = (direction) => {
+    const container = containerRef.current;
+
+    if (container) {
+      const scrollAmount = 295;
+      const currentScroll = container.scrollLeft;
+
+      if (direction === 'left') {
+        container.scrollLeft = currentScroll - scrollAmount;
+      } else if (direction === 'right') {
+        container.scrollLeft = currentScroll + scrollAmount;
+      }
+    }
+  };
+
   return (
     <div className={styles.listBackground}>
       <div className={styles.cardListForm}>
         <h1>인기 롤링 페이퍼 🔥</h1>
-        <div>card list</div>
+        <div ref={containerRef} className={styles.cardListItem}>
+          <CardlistTest />
+        </div>
       </div>
+      <button onClick={() => handleScroll('left')}>왼쪽으로 이동</button>
+      <button onClick={() => handleScroll('right')}>오른쪽으로 이동</button>
       <div className={styles.cardListForm}>
         <h1>최근에 만든 롤링 페이퍼 ⭐️️</h1>
-        <div>card list</div>
+        <div className={styles.cardListItem}>
+          <CardlistTest />
+        </div>
       </div>
       <div className={styles.listBottom}>
         <Button
