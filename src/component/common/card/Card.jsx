dabd35049cda36adb4709094bd7b7084dcae909e
soohwan93 <soohwan2im@gmail.com>
2024-03-04 14:09:26 +0900

    Feat: 모달창과 연동하여 모달창 보이는 기능 추가

--- src/component/common/card/Card.jsx
+++ src/component/common/card/Card.jsx
@@ -3,13 +3,26 @@ import Button from "../button/Button";
 import styles from "./Card.module.css";
 import DOMPurify from "dompurify";
 import { fontMappings } from "../textField/selectBox/fontMappings";
+import { useState } from "react";
+import ModalPortal from "../modal/ModalPortal";
+import { Modal } from "../modal/Modal";
 function Card({ messages, isAddMessageCardVisible, image }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [modalItem, setModalItem] = useState(null);
   const navigate = useNavigate();
+  const handleCardModalClick = (item) => {
+    setModalItem(item);
+    setIsModalVisible(!isModalVisible);
+  };
   const handleAddMessageButtonClick = () => {
     navigate("message");
   };
+  const handlePreventRightClick = (e) => {
+    e.preventDefault();
+  };
   return (
     <div
+      onContextMenu={handlePreventRightClick}
       className={styles.backGround}
       style={
         image?.includes("http")
@@ -35,42 +48,57 @@ function Card({ messages, isAddMessageCardVisible, image }) {
           </div>
         )}
         {messages?.map((item) => (
-          <div key={item.id} className={styles.cardContainer}>
-            <div className={styles.profileContainer}>
-              <div className={styles.profile}>
-                <div
-                  className={styles.img}
-                  style={{ backgroundImage: `url(${item.profileImageURL})` }}
-                />
-                <div className={styles.nameAndBadgeContainer}>
-                  <div className={styles.nameContainer}>
-                    <span>From.</span>
-                    <span>{item.sender}</span>
+          <>
+            <div
+              onClick={() => handleCardModalClick(item)}
+              key={item.id}
+              className={styles.cardContainer}
+            >
+              <div className={styles.profileContainer}>
+                <div className={styles.profile}>
+                  <div
+                    className={styles.img}
+                    style={{ backgroundImage: `url(${item.profileImageURL})` }}
+                  />
+                  <div className={styles.nameAndBadgeContainer}>
+                    <div className={styles.nameContainer}>
+                      <span>From.</span>
+                      <span>{item.sender}</span>
+                    </div>
+                    <div className={styles.badge}>
+                      <div className={styles.badgeText}>
+                        {item.relationship}
+                      </div>
+                    </div>
                   </div>
-                  <div className={styles.badge}>
-                    <div className={styles.badgeText}>{item.relationship}</div>
-                  </div>
+                  {!isAddMessageCardVisible && (
+                    <div className={styles.trashButton}>
+                      <Button type="outlined" height="standard" icon="delete" />
+                    </div>
+                  )}
                 </div>
-                {!isAddMessageCardVisible && (
-                  <div className={styles.trashButton}>
-                    <Button type="outlined" height="standard" icon="delete" />
-                  </div>
-                )}
+                <div className={styles.line} />
               </div>
-              <div className={styles.line} />
+              <div className={styles.textAreaContainer}>
+                <div
+                  className={styles.textBox}
+                  style={{ fontFamily: `${fontMappings[item.font]}` }}
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(item.content),
+                  }}
+                />
+                <span className={styles.date}>
+                  {item.createdAt.slice(0, 10)}
+                </span>
+              </div>
             </div>
-            <div className={styles.textAreaContainer}>
-              <div
-                className={styles.textBox}
-                style={{ fontFamily: `${fontMappings[item.font]}` }}
-                dangerouslySetInnerHTML={{
-                  __html: DOMPurify.sanitize(item.content),
-                }}
-              />
-              <span className={styles.date}>{item.createdAt.slice(0, 10)}</span>
-            </div>
-          </div>
+          </>
         ))}
+        <ModalPortal>
+          {isModalVisible && (
+            <Modal item={modalItem} onClick={handleCardModalClick} />
+          )}
+        </ModalPortal>
       </div>
     </div>
   );
