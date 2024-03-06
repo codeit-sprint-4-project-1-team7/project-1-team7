180a45fd3057e4f0338df5bb5ad1271d35006b63
ayoung-iya <rhaehfdl1879@naver.com>
2024-03-06 13:46:56 +0900

    Refactor: 빈 태그 제거

--- src/component/common/header/ProfileImages.jsx
+++ src/component/common/header/ProfileImages.jsx
@@ -2,23 +2,21 @@ import styles from "./ProfileImages.module.css";
 
 function ProfileImages({ direction, messageCount, recentMessages }) {
   return (
-    <>
-      <div className={`${styles.profileGroup} ${styles[direction]}`}>
-        {recentMessages.map(({ id, profileImageURL }) => (
-          <img
-            key={id}
-            className={styles.profileImg}
-            src={profileImageURL}
-            alt="프로필 사진"
-          />
-        ))}
-        {messageCount > 3 && (
-          <span id="imageText" className={styles.profileCount}>
-            +{+messageCount - 3}
-          </span>
-        )}
-      </div>
-    </>
+    <div className={`${styles.profileGroup} ${styles[direction]}`}>
+      {recentMessages.map(({ id, profileImageURL }) => (
+        <img
+          key={id}
+          className={styles.profileImg}
+          src={profileImageURL}
+          alt="프로필 사진"
+        />
+      ))}
+      {messageCount > 3 && (
+        <span id="imageText" className={styles.profileCount}>
+          +{+messageCount - 3}
+        </span>
+      )}
+    </div>
   );
 }
 
