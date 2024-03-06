68d87bb1322e2f6f9fd0a5199b51af279e3db50c
soohwan93 <soohwan2im@gmail.com>
2024-03-06 14:07:02 +0900

    Design: notFound 페이지 디자인

--- src/component/pages/NotFound.jsx
+++ src/component/pages/NotFound.jsx
@@ -1,3 +1,12 @@
+import image from "../../asset/img/notFoundImg/notFound_Img.png";
+import styles from "./NotFound.module.css";
 export function NotFound() {
-  return <div>페이지가 없거나 잘못된 요청입니다.</div>;
+  return (
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <img src={image} alt="notFound" height="600px" />
+        <div className={styles.text}>페이지가 없거나 잘못된 요청입니다</div>
+      </div>
+    </div>
+  );
 }
