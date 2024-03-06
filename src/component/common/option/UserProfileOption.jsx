b2e42c35c630a1bf68f29044f522aaa74e97333e
choiwonseokgit <cs0328@hanmail.net>
2024-03-06 13:54:29 +0900

    Refactor: UserProfileOption 컴포넌트 내upLoadImg 함수 util 에서 import 로 변경

--- src/component/common/option/UserProfileOption.jsx
+++ src/component/common/option/UserProfileOption.jsx
@@ -1,30 +1,9 @@
 import React, { useRef } from "react";
 import styles from "./UserProfileOption.module.css";
+import { upLoadImg } from "../../../util/api";
 
 function UserProfileOption({ currentProfileImg, onChangeProfileImg }) {
   const fileInput = useRef(null);
-
-  const upLoadImg = async (imgFile) => {
-    try {
-      const formData = new FormData();
-      formData.append("image", imgFile);
-      const response = await fetch(
-        "https://api.imgbb.com/1/upload?key=d0683b0869118bab9113ca272a7d46b1",
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
-
-      if (!response?.ok) {
-        throw new Error("이미지를 업로드 하는 데 실패했습니다.");
-      }
-      const data = await response.json();
-      return data.data.url;
-    } catch (error) {
-      console.error("Error uploading image:", error);
-    }
-  };
 
   const handleAddImageDataChange = async (e) => {
     const { files } = e.target;
