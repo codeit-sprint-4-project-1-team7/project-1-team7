cda2918b4daac7e9e3948c5b83e03d10c4560df7
choiwonseokgit <cs0328@hanmail.net>
2024-03-07 14:11:46 +0900

    Feat: Input 컴포넌트 maxlength 20자 제한으로 수정

--- src/component/common/textField/input/Input.jsx
+++ src/component/common/textField/input/Input.jsx
@@ -46,6 +46,7 @@ function Input({
         placeholder={placeHolderType}
         value={inputValue}
         onChange={handleonChangeValue}
+        maxlength="20"
       />
       {isError && <div className={styles.errorMessage}>값을 입력해 주세요</div>}
     </div>
