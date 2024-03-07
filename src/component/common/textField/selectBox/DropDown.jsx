e59a7d3e38ab9b298ffbf27f6df0f276616a8c16
choiwonseokgit <cs0328@hanmail.net>
2024-03-07 13:13:59 +0900

    Feat: SelectBox 폰트 타입일시 DropDown에 폰트 적용되게 수정

--- src/component/common/textField/selectBox/DropDown.jsx
+++ src/component/common/textField/selectBox/DropDown.jsx
@@ -1,8 +1,13 @@
 import styles from "./DropDown.module.css";
+import { fontMappings } from "./fontMappings";
 
 function DropDown({ select, onClick }) {
   return (
-    <li className={styles.li} onClick={onClick}>
+    <li
+      className={styles.li}
+      onClick={onClick}
+      style={{ fontFamily: fontMappings[select] || "Pretendard" }}
+    >
       {select}
     </li>
   );
