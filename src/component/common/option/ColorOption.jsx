f04882a9b420a167972703d09c78a43d01ae4f67
양서연 <ysynop7@naver.com>
2024-03-18 16:13:25 +0900

    Refactor: ColorOption 구조를 map 배열 렌더링으로 변경

--- src/component/common/option/ColorOption.jsx
+++ src/component/common/option/ColorOption.jsx
@@ -7,41 +7,18 @@ function ColorOption({ clickItem, onClick }) {
 
   return (
     <div className={styles.backgroundOptions}>
-      <button
-        type='button'
-        value={COLOR_NAME[0]}
-        className={`${styles.option} ${styles.colorBeige}`}
-        onClick={onClick}>
-        {COLOR_NAME[0] === clickItem
-          && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
-      </button>
-
-      <button
-        type='button'
-        value={COLOR_NAME[1]}
-        className={`${styles.option} ${styles.colorPurple}`}
-        onClick={onClick}>
-        {COLOR_NAME[1] === clickItem
-          && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
-      </button>
-
-      <button
-        type='button'
-        value={COLOR_NAME[2]}
-        className={`${styles.option} ${styles.colorBlue}`}
-        onClick={onClick}>
-        {COLOR_NAME[2] === clickItem
-          && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
-      </button>
-
-      <button
-        type='button'
-        value={COLOR_NAME[3]}
-        className={`${styles.option} ${styles.colorGreen}`}
-        onClick={onClick}>
-        {COLOR_NAME[3] === clickItem
-          && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
-      </button>
+      {COLOR_NAME.map((item) => {
+        return (
+          <button
+            type='button'
+            value={item}
+            className={`${styles.option} ${styles[item]}`}
+            onClick={onClick}>
+            {item === clickItem
+              && <img className={styles.backgroundChecked} src={checkIcon} alt="check" />}
+          </button>
+        )
+      })}
     </div>
   );
 }
